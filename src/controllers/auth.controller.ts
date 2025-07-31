a85cbede6768580f2c5f7b900bc8438757d2d3a6
Nooriman Noor <nooriman.mdnoor@gmail.com>
2025-07-31 16:26:29 +0800

    feat: login controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import prisma from "../utils/prisma";
-import { hashPassword } from "../utils/bcrypt-hash";
+import { comparePassword, hashPassword } from "../utils/bcrypt-hash";
+import generateToken from "../utils/generateToken";
 
 export class AuthController {
   constructor() {}
@@ -10,14 +11,29 @@ export class AuthController {
     const { email, password } = req.body;
 
     if (!email || !password)
-      res
-        .status(StatusCodes.OK)
-        .json({ message: "Please enter username or password" });
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "Please enter email or password" });
 
     try {
-      res.setHeader("authorization", "123");
-      res.cookie("refresh_token", "1111", {
-        maxAge: Number(process.env.JWT_EXPIRE_HR) * 60 * 1000,
+      const user = await prisma.profile.findFirst({
+        where: { email },
+      });
+
+      if (!user) throw new Error("User is not found");
+
+      const passwordMatched = await comparePassword(password, user.password);
+
+      if (!passwordMatched)
+        return res
+          .status(StatusCodes.UNAUTHORIZED)
+          .json({ message: "Wrong Password. Please try again" });
+
+      const { accessToken, refreshToken } = generateToken(user);
+
+      res.setHeader("authorization", accessToken);
+      res.cookie("refresh_token", refreshToken, {
+        maxAge: Number(process.env.JWT_EXPIRE_HR) * 60 * 60 * 1000,
         secure: false, // set true in prod
         httpOnly: true,
         sameSite: "lax", // set strict in prod
