01e070e5e24b197abed3916126186985d3d3f168
Nooriman Noor <nooriman.mdnoor@gmail.com>
2025-07-31 14:56:02 +0800

    feat: added default admin user sql

--- src/sql/default.sql
+++ src/sql/default.sql
@@ -3,3 +3,21 @@ VALUES
   ('admin'),
   ('user')
 ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO "Profile" (
+  "id",
+  "firstName",
+  "lastName",
+  "email",
+  "password",
+  "createdAt",
+  "roleId"
+) VALUES (
+  gen_random_uuid(),
+  'Admin',
+  'User',
+  'admin@admin.com',
+  '$2b$10$qKwUlH/N0pL.kHdEx6EGqOrU5QfsZfnwGuWsvXpkA5RbRMQThh1ne',
+  NOW(),
+  1
+);
