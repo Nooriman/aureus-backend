fbf0cc47935553d462bf27302bd2629a90633505
Nooriman Noor <nooriman.mdnoor@gmail.com>
2025-08-01 00:45:57 +0800

    feat: profile details controller

--- src/controllers/profile.controller.ts
+++ src/controllers/profile.controller.ts
@@ -1,12 +1,28 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
+import { getUserFromToken } from "../utils/getUserFromToken";
+import prisma from "../utils/prisma";
 
 export class ProfileController {
-  constructor() { }
+  constructor() {}
 
   async getProfileDetails(req: Request, res: Response) {
     try {
-      res.status(StatusCodes.OK).json({ message: "123" });
+      const token = req.headers.authorization;
+      const data = getUserFromToken(token);
+
+      const userProfile = await prisma.profile.findUnique({
+        where: { id: data?.id },
+        include: { role: true },
+      });
+
+      if (!userProfile) {
+        return res
+          .status(StatusCodes.NOT_FOUND)
+          .json({ message: "Profile not found" });
+      }
+
+      res.status(StatusCodes.OK).json(userProfile);
     } catch (error: any) {
       res
         .status(StatusCodes.INTERNAL_SERVER_ERROR)
--- src/routes/profile.route.ts
+++ src/routes/profile.route.ts
@@ -4,7 +4,7 @@ import { ProfileController } from "../controllers/profile.controller";
 const router = Router();
 const profileController = new ProfileController();
 
-router.get("/profile-detail", (req: Request, res: Response) => {
+router.get("/profile-details", (req: Request, res: Response) => {
   profileController.getProfileDetails(req, res);
 });
 
